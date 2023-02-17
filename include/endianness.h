071ac57a03df7cbe9bf244e22e288030ca41de34
Sonu Aryan <57495903+cosmo-developer@users.noreply.github.com>
2023-02-18 00:05:09 +0530

    welcome

--- include/endianness.h
+++ include/endianness.h
@@ -1,242 +1,109 @@
-#pragma once
-#ifndef ENDIANNESS_H
-#define ENDIANNESS_H
-/* Public domain implementation for endianness detection and byte ordering on
-   several platforms. In case the concept of public domain does not exist
-   under your jurisdiction, you can consider it to be dual licensed
-   under the MIT, Apache and WTFPL licenses.
-   Grab it and drop it into your project, include it and use
-   the following macros to determine endianness:
-   ENDIANNESS_LE, ENDIANNESS_BE
-   e.g. #if ENDIANNESS_LE ...
-   or, even nicer without littering your code with #ifdefs:
-   if (ENDIANNESS_BE) { big_endian_code(); } else { little_endian_code(); }
-   ... since the compiler can optimize away unused branches, this makes your
-   code easier to read while not loosing any of the advantage of using
-   conditional compilation, plus you get a free compile-time check of the
-   unused code path (rarely used conditonally compiled code paths often get
-   defunct over time if nobody checks them all the time).
-   To debug this header yourself, you can define ENDIANNESS_DEBUG to see
-   warnings from where we take the defs for the specific target.
-   If you need only the conversion functions from big to little endian
-   and vice versa, you may want to #define ENDIANNESS_PORTABLE_CONVERSION
-   prior to including this header. That way, when the endiannes can't be
-   determined at compile time, the code will fallback to a slower,
-   but portable version of those functions.
-   However, if using it, it's not guaranteed that ENDIANNESS_LE/BE
-   will be defined.
-   Most people however need only the conversion functions in their code,
-   so if you stick to them you can safely turn the portable conversion on.
+#ifndef ENDIANESS_H
+#define ENDIANESS_H
+/**
+* LICENSE
+* 
+* BSD 3-Clause License
+
+Copyright (c) 2023, Sonu Aryan
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+#ifdef __GNUC__
+    // code for GCC and related compilers
+	#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    // code for big-endian systems
+		#define MBIG_ENDIAN
+	#elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+		// code for little-endian systems
+		#define MLITTLE_ENDIAN
+	#endif
 
-/* This catches all modern GCCs (>= 4.6) and Clang (>=3.2) */
-#if (defined __BYTE_ORDER__) && (defined __ORDER_LITTLE_ENDIAN__)
-# ifdef ENDIANNESS_DEBUG
-#  warning "Taking endiannes from built-in __BYTE_ORDER__"
-# endif
-# if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-#  define ENDIANNESS_LE 1
-#  define ENDIANNESS_BE 0
-# elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-#  define ENDIANNESS_LE 0
-#  define ENDIANNESS_BE 1
-# endif
-/* Try to derive from arch/compiler-specific macros */
-#elif defined(_X86_) || defined(__x86_64__) || defined(__i386__) || \
-      defined(__i486__) || defined(__i586__) || defined(__i686__) || \
-      defined(__MIPSEL) || defined(_MIPSEL) || defined(MIPSEL) || \
-      defined(__ARMEL__) || \
-      defined(__MSP430__) || \
-      (defined(__LITTLE_ENDIAN__) && __LITTLE_ENDIAN__ == 1) || \
-      (defined(_LITTLE_ENDIAN) && _LITTLE_ENDIAN == 1) || \
-      defined(_M_ARM) || defined(_M_ARM64) || \
-      defined(_M_IX86) || defined(_M_AMD64) /* MSVC */
-# ifdef ENDIANNESS_DEBUG
-# warning "Detected Little Endian target CPU"
-# endif
-# define ENDIANNESS_LE 1
-# define ENDIANNESS_BE 0
-#elif defined(__MIPSEB) || defined(_MIPSEB) || defined(MIPSEB) || \
-      defined(__MICROBLAZEEB__) || defined(__ARMEB__) || \
-      (defined(__BIG_ENDIAN__) && __BIG_ENDIAN__ == 1) || \
-      (defined(_BIG_ENDIAN) && _BIG_ENDIAN == 1)
-# ifdef ENDIANNESS_DEBUG
-#  warning "Detected Big Endian target CPU"
-# endif
-# define ENDIANNESS_LE 0
-# define ENDIANNESS_BE 1
-/* Try to get it from a header */
+#elif defined(_MSC_VER)
+    // code for Microsoft Visual C++
+#if defined(_M_IX86) || defined(_M_AMD64) || defined(_M_IA64) || defined(_M_ARM) || defined(_M_ARM_NT) || defined(_M_ARM64)
+	#define MLITTLE_ENDIAN
+#elif defined(_M_MRX000) || defined(_MIPS_)
+	#ifdef _MIPSEL
+		#define MLITTLE_ENDIAN
+	#else
+		#define MBIG_ENDIAN
+	#endif
+
+#elif defined(_M_PPC) || defined(_M_PPCBE)
+	#define MBIG_ENDIAN
+#endif
+
+
+	
+#elif defined(__clang__)
+    // code for Clang
+	// code for GCC and related compilers
+	#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    // code for big-endian systems
+		#define MBIG_ENDIAN
+	#elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+		// code for little-endian systems
+		#define MLITTLE_ENDIAN
+	#endif
+	
+#elif defined(__INTEL_COMPILER)
+    // code for Intel C++ Compiler
+	#ifdef _BIG_ENDIAN
+    // code for big-endian systems
+		#define MBIG_ENDIAN
+	#else
+		// code for little-endian systems
+		#define MLITTLE_ENDIAN
+	#endif
+
+#elif defined(__IBMC__) || defined(__IBMCPP__)
+    // code for IBM XL C/C++ Compiler
+	#ifdef _LITTLE_ENDIAN
+    // code for little-endian systems
+		#define MLITTLE_ENDIAN
+	#else
+		// code for big-endian systems
+		#define MBIG_ENDIAN
+	#endif
+
+#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+    #ifdef __LITTLE_ENDIAN
+    // code for little-endian systems
+		#define MLITTLE_ENDIAN
+	#else
+		// code for big-endian systems
+		#define MBIG_ENDIAN
+	#endif
+
+#elif defined(__TINYC__)
+    // code for Tiny C Compiler (TCC)
+	#error "Not available yet"
 #else
-# if defined(__linux) || defined(__HAIKU__)
-#  ifdef ENDIANNESS_DEBUG
-#   warning "Taking endiannes from endian.h"
-#  endif
-#  include <endian.h>
-# elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || \
-       defined(__DragonFly__)
-#  ifdef ENDIANNESS_DEBUG
-#   warning "Taking endiannes from sys/endian.h"
-#  endif
-#  include <sys/endian.h>
-# elif defined(__APPLE__)
-#  ifdef ENDIANNESS_DEBUG
-#   warning "Taking endiannes from machine/endian.h"
-#  endif
-#  include <machine/endian.h>
-# endif
+    #error Unsupported compiler
 #endif
-
-#ifndef ENDIANNESS_LE
-# undef ENDIANNESS_BE
-# if defined(__BYTE_ORDER) && defined(__LITTLE_ENDIAN)
-#  if __BYTE_ORDER == __LITTLE_ENDIAN
-#   define ENDIANNESS_LE 1
-#   define ENDIANNESS_BE 0
-#  elif __BYTE_ORDER == __BIG_ENDIAN
-#   define ENDIANNESS_LE 0
-#   define ENDIANNESS_BE 1
-#  endif
-# elif defined(BYTE_ORDER) && defined(LITTLE_ENDIAN)
-#  if BYTE_ORDER == LITTLE_ENDIAN
-#   define ENDIANNESS_LE 1
-#   define ENDIANNESS_BE 0
-#  elif BYTE_ORDER == BIG_ENDIAN
-#   define ENDIANNESS_LE 0
-#   define ENDIANNESS_BE 1
-#  endif
-# endif
-#endif
-
-/* In case the user passed one of -DENDIANNESS_LE or BE in CPPFLAS,
-   set the second one too */
-#if defined(ENDIANNESS_LE) && !(defined(ENDIANNESS_BE))
-# if ENDIANNESS_LE == 0
-#  define ENDIANNESS_BE 1
-# else
-#  define ENDIANNESS_BE 0
-# endif
-#elif defined(ENDIANNESS_BE) && !(defined(ENDIANNESS_LE))
-# if ENDIANNESS_BE == 0
-#  define ENDIANNESS_LE 1
-# else
-#  define ENDIANNESS_LE 0
-# endif
-#endif
-
-#if !(defined(ENDIANNESS_LE)) && !(defined(ENDIANNESS_PORTABLE_CONVERSION))
-# error "Sorry, we couldn't detect endiannes for your system! Please set -DENDIANNESS_LE=1 or 0 using your CPPFLAGS/CFLAGS and open an issue for your system on https://github.com/rofl0r/endianness.h - Thanks!"
-#endif
-
-#ifdef ENDIANNESS_DEBUG
-# if ENDIANNESS_LE == 1
-    #  warning "Detected Little Endian target CPU"
-# endif
-# if ENDIANNESS_BE == 1
-    #  warning "Detected BIG Endian target CPU"
-# endif
-#endif
-
-#include <stdint.h>
-
-    static __inline uint16_t end_bswap16(uint16_t __x)
-{
-    return (__x << 8) | (__x >> 8);
-}
-
-static __inline uint32_t end_bswap32(uint32_t __x)
-{
-    return (__x >> 24) | (__x >> 8 & 0xff00) | (__x << 8 & 0xff0000) | (__x << 24);
-}
-
-static __inline uint64_t end_bswap64(uint64_t __x)
-{
-    return ((end_bswap32((uint32_t)__x) + 0ULL) << 32) | (end_bswap32(__x >> 32));
-}
-
-
-static __inline uint16_t end_htobe16_impl(uint16_t n)
-{
-    union { int i; char c; } u = { 1 };
-    return u.c ? end_bswap16(n) : n;
-}
-
-static __inline uint16_t end_htole16_impl(uint16_t n)
-{
-    union { int i; char c; } u = { 1 };
-    return u.c ? n : end_bswap16(n);
-}
-
-static __inline uint32_t end_htobe32_impl(uint32_t n)
-{
-    union { int i; char c; } u = { 1 };
-    return u.c ? end_bswap32(n) : n;
-}
-
-static __inline uint32_t end_htole32_impl(uint32_t n)
-{
-    union { int i; char c; } u = { 1 };
-    return u.c ? n : end_bswap32(n);
-}
-
-static __inline uint64_t end_htobe64_impl(uint64_t n)
-{
-    union { int i; char c; } u = { 1 };
-    return u.c ? end_bswap64(n) : n;
-}
-
-static __inline uint64_t end_htole64_impl(uint64_t n)
-{
-    union { int i; char c; } u = { 1 };
-    return u.c ? n : end_bswap64(n);
-}
-
-#if ENDIANNESS_LE+0 == 1
-# define end_htobe16(x) end_bswap16(x)
-# define end_be16toh(x) end_bswap16(x)
-# define end_htobe32(x) end_bswap32(x)
-# define end_be32toh(x) end_bswap32(x)
-# define end_htobe64(x) end_bswap64(x)
-# define end_be64toh(x) end_bswap64(x)
-# define end_htole16(x) (uint16_t)(x)
-# define end_le16toh(x) (uint16_t)(x)
-# define end_htole32(x) (uint32_t)(x)
-# define end_le32toh(x) (uint32_t)(x)
-# define end_htole64(x) (uint64_t)(x)
-# define end_le64toh(x) (uint64_t)(x)
-#elif ENDIANNESS_BE+0 == 1
-# define end_htobe16(x) (uint16_t)(x)
-# define end_be16toh(x) (uint16_t)(x)
-# define end_htobe32(x) (uint32_t)(x)
-# define end_be32toh(x) (uint32_t)(x)
-# define end_htobe64(x) (uint64_t)(x)
-# define end_be64toh(x) (uint64_t)(x)
-# define end_htole16(x) end_bswap16(x)
-# define end_le16toh(x) end_bswap16(x)
-# define end_htole32(x) end_bswap32(x)
-# define end_le32toh(x) end_bswap32(x)
-# define end_htole64(x) end_bswap64(x)
-# define end_le64toh(x) end_bswap64(x)
-#else
-/* Resort to slower, but neutral code */
-# define end_htobe16(x)  end_htobe16_impl(x)
-# define end_be16toh(x)  end_htobe16_impl(x)
-# define end_htobe32(x)  end_htobe32_impl(x)
-# define end_be32toh(x)  end_htobe32_impl(x)
-# define end_htobe64(x)  end_htobe64_impl(x)
-# define end_be64toh(x)  end_htobe64_impl(x)
-# define end_htole16(x)  end_htole16_impl(x)
-# define end_le16toh(x)  end_htole16_impl(x)
-# define end_htole32(x)  end_htole32_impl(x)
-# define end_le32toh(x)  end_htole32_impl(x)
-# define end_htole64(x)  end_htole64_impl(x)
-# define end_le64toh(x)  end_htole64_impl(x)
-#endif
-
-#define end_ntoh16(x)  end_be16toh(x)
-#define end_hton16(x)  end_htobe16(x)
-#define end_ntoh32(x)  end_be32toh(x)
-#define end_hton32(x)  end_htobe32(x)
-#define end_ntoh64(x)  end_be64toh(x)
-#define end_hton64(x)  end_htobe64(x)
 
 #endif
